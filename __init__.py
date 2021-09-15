b5ed875959d3896f619ec3183a52c3fd7efb0cec
sumanbgl <suman.bgl@gmail.com>
2021-09-15 21:02:38 +0530

    Add files via upload
    service bus trigger code

--- __init__.py
+++ __init__.py
@@ -1,8 +1,10 @@
 import logging
-
+import os
 import azure.functions as func
 import psycopg2
 from datetime import  datetime as dt
+from sendgrid import SendGridAPIClient
+from sendgrid.helpers.mail import Mail
 
 def main(msg: func.ServiceBusMessage):
     logging.info('Python ServiceBus queue trigger processed message: %s',
@@ -21,20 +23,20 @@ def main(msg: func.ServiceBusMessage):
 
         cur = conn.cursor()
         
-        notifications = fetch_notification_by_id(notification_id, cur)
-
-        for notification in notifications:
-            logging.info("{}, {}, {}, {}", notification.status, notification.message, notification.submitted_date, notification.subject)
-
-        existing_notification = notifications[0]
-
+        logging.info('type of notification id: %s', type(notification_id))
+        notification = fetch_notification_by_id(notification_id, cur)
+                    
+        logging.info(notification)
+       
         attendees = fetch_attendees(cur)
         for attendee in attendees:
-            logging.info("{}, {}, {}", attendee.last_name, attendee.first_name, attendee.email)
+            logging.info(attendee)
+            subject = '{}: {}'.format(attendee.first_name, notification.subject)
+            send_email(attendee.email, subject, notification.message)
 
         status = 'Notified {} attendees'.format(len(attendees))
-        logging.info('Status : %s',status)
-        insert_notification(existing_notification, status, cur, conn)
+        logging.info('Status : %s',status)        
+        update_notification_status_completed_date(status, dt.utcnow(), notification_id, cur, conn)
 
         # close the communication with the PostgreSQL
         cur.close()
@@ -49,18 +51,16 @@ def main(msg: func.ServiceBusMessage):
 
 
 def fetch_notification_by_id(id, cur):
-    logging.info('fetch_notification_by_id')
-    query = 'SELECT STATUS, MESSAGE, SUBMITTED_DATE, SUBJECT FROM NOTIFICATION WHERE ID=%s'
-    # cur.execute('SELECT MESSAGE, SUBJECT FROM NOTIFICATION WHERE ID=3')
-    cur.execute(query, (id,))
-    rows = cur.fetchall()
-    notifications = []
+    logging.info('fetch_notification_by_id: id - %s', str(id))    
+    cur.execute("SELECT MESSAGE, SUBJECT FROM NOTIFICATION WHERE ID = %s",(id,))
+    row = cur.fetchone()
+    notification = Notification(row[0], row[1])
+    return notification
+    # for row in rows:
+    #     notification = Notification(row[0], row[1])
+    #     notifications.append(notification)
 
-    for row in rows:
-        notification = Notification(row[0], row[1], row[2], row[3])
-        notifications.append(notification)
-
-    return notifications
+    # return notifications
 
 def fetch_attendees(cur):
     cur.execute('SELECT FIRST_NAME, LAST_NAME, EMAIL FROM ATTENDEE')
@@ -73,21 +73,43 @@ def fetch_attendees(cur):
 
     return  attendees
 
-def insert_notification(existing_notification, status, cur, conn):
-    insert_sql = 'INSERT INTO NOTIFICATION (STATUS, MESSAGE, SUBMITTED_DATE, COMPLETED_DATE, SUBJECT) VALUES (%s, %s, %s, %s, %s)'
-    cur.execute(insert_sql, (status, existing_notification.message, existing_notification.submitted_date, str(dt.utcnow()), existing_notification.subject,))
+def update_notification_status_completed_date(status, completed_date, id, cur, conn):
+    update_sql = 'UPDATE NOTIFICATION SET STATUS = %s, COMPLETED_DATE = %s WHERE ID = %s'
+    cur.execute(update_sql, (status, completed_date, id,))
     conn.commit()
+    logging.info("update successful")
 
+def send_email(email, subject, body):
+    send_grid_api_key = os.environ['SENDGRID_API_KEY']
+    if send_grid_api_key:
+        try:
+            email_msg = Mail(
+                from_email=os.environ['ADMIN_EMAIL_ADDRESS'],
+                to_emails=email,
+                subject=subject,
+                plain_text_content=body)
+
+            email_client = SendGridAPIClient(send_grid_api_key)
+            email_client.send(email_msg)
+            logging.info("Successfully notified %s", email)
+        except Exception as ex:
+            logging.error(ex)            
+
+    
 
 class Attendee(object):
     def __init__(self, fname, lname, email):
         self.first_name = fname
         self.last_name = lname
         self.email = email
+    
+    def __str__(self):
+        return f'Attendee Info: {self.last_name} , {self.first_name}, {self.email}'        
 
 class Notification(object):
-    def __init__(self, status, msg, submitted_date, sub):
-        self.status = status
-        self.message = msg
-        self.submitted_date = submitted_date
+    def __init__(self, msg, sub):       
+        self.message = msg        
         self.subject = sub
+
+    def __str__(self):
+        return f'Notification: {self.message} , {self.subject}'
